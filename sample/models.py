ebc0b521d1f680797a0171a975f98fd53a20ce48
H2W-DEV\nakanishi <t-nakanishi@h2-works.co.jp>
2024-03-29 09:20:16 +0900

    djangoproject作成

---
+++ sample/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+import uuid
+
+class SampleModel(models.Model):
+    class Meta:
+        db_table = 'sample'
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
